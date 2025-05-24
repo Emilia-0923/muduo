182bfd8726d3aeda120a0b4b1f0c1f4a3f497da9
Emilia-0923 <love-emilia@foxmail.com>
2025-05-24 15:00:11 +0800

    gitignore

---
+++ build/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/home/emilia/Disk/Code/project/Http_Server/source/Main.cpp.o"
+  "CMakeFiles/Server.dir/home/emilia/Disk/Code/project/Http_Server/source/Main.cpp.o.d"
+  "Server"
+  "Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
